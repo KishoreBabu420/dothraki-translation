bb0408721f620a30fdbe96258c217cc70b6330d4
Kishore <kishore.sai37@gmail.com>
2021-06-17 22:12:58 +0530

    logic complted

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 'use strict';
+const input = document.getElementById('input');
+
+const btnTranslate = document.getElementById('btn-translate');
+
+const output = document.getElementById('output');
+
+let url = 'https://api.funtranslations.com/translate/dothraki.json';
+
+async function translate(text) {
+  const data = await fetch(`${url}?text=${text}`);
+  const result = await data.json();
+  output.innerHTML = result.contents.translated;
+}
+
+btnTranslate.addEventListener('click', () => {
+  const translatedText = translate(input.value.trim());
+});
